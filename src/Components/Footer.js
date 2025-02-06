8c1a4ddd39bf5f3311249351460b92c08f533e5b
MarjanSaedi <marjansaedi@gmail.com>
2025-02-06 11:44:43 -0700

    Footer is added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,14 @@
 import React from "react";
+import footerImage from "../photos/footer.png";
 
 import "./Footer.css";
 
 function Footer() {
-  return <footer className="header-container">Footer</footer>;
+  return (
+    <footer className="header-container">
+      <img src={footerImage} alt="Footer" className="top-image" />
+    </footer>
+  );
 }
 
 export default Footer;
